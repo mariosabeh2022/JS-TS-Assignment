d9052ec5043ac099bbd20b18eb980914b2a289bd
mariosabeh2022 <mariosabeh2022@gmail.com>
2025-03-21 11:35:43 +0200

    Adding storing validation for the challenge 3

--- Challenge3.ts
+++ Challenge3.ts
@@ -1,5 +1,15 @@
 //This function also works with a series of numbers from N to M
 function findMissingNumber(arr: number[]): number  {
+    //Validating if the array is sorted
+    for (let i=0;i<arr.length-1;i++){
+        for (let k=0;k<arr.length-i-1;k++){
+            if (arr[k]>arr[k+1]){
+                let temp=arr[k];
+                arr[k]=arr[k+1];
+                arr[k+1]=temp;
+            }
+        }
+    }
     let firstElement = arr[0];
     for (let i = 0; i < arr.length; i++){
         if (arr[i] != firstElement)
